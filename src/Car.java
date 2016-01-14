2cd1b211136ede0f53820b54bb7488fd1b60da58
MikeKwon36 <mikeykwon@gmail.com>
2016-01-14 12:38:05 -0500

    GitLab2

---
+++ src/Car.java
@@ -0,0 +1,6 @@
+/**
+ * Created by YPsitos on 1/14/16.
+ */
+public class Car {
+    String mWheels, mDoors, mEngine;
+}
---
+++ src/Garage.java
@@ -0,0 +1,5 @@
+/**
+ * Created by Todo on 1/14/2016.
+ */
+public class Garage {
+}
